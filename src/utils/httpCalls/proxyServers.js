5011070caa6d845fbe40f5e3c7caa9aa04b51fac
michaelftout <toutonghi.michael@gmail.com>
2019-05-29 10:28:13 -0700

    Switch back to https to all iOS versions and andriod version newer than API 24

--- src/utils/httpCalls/proxyServers.js
+++ src/utils/httpCalls/proxyServers.js
@@ -3,8 +3,6 @@ import { Platform } from 'react-native';
 const httpsServers = ['el1.vrsc.0x03.services', 'el2.vrsc.0x03.services'];
 const httpServers = ['94.130.225.86:80', '94.130.225.86:80'];
 
-export const httpsEnabled = false//Platform.OS === 'android' && Platform.Version <= 24 ? false : true;
+export const httpsEnabled = Platform.OS === 'android' && Platform.Version <= 24 ? false : true;
 
-export const proxyServers = httpServers//httpsEnabled ? httpsServers : httpServers;
-
-//TODO: Setup https servers
+export const proxyServers = httpsEnabled ? httpsServers : httpServers;
