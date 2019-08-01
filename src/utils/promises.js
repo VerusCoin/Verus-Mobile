cf54486e2e5674c36d3f31c9c56c4ea3312b723d
michaelftout <toutonghi.michael@gmail.com>
2019-08-01 13:53:25 -0700

    Add util function to resolve promises in an array sequentially

--- src/utils/promises.js
+++ src/utils/promises.js
@@ -6,3 +6,21 @@ export const timeout = (ms, promise) => {
     promise.then(resolve, reject)
   })
 }
+
+/**
+ * Takes in an array of promises and resolves them sequentially, returning a promise that
+ * resolves with an array of promise resolutions (or rejections). Any non-promise types 
+ * in the array are left alone in their index for the return array.
+ * @param {Array} promises An array of any type
+ */
+export const resolveSequentially = (promises) => {
+	return promises.reduce((accumulator, promise) => {
+		return accumulator.then(arr => {
+    	if (!promise.then) return arr.concat(promise)
+			return promise.then(res => {
+      	return arr.concat(res)
+      })
+		})
+	},
+  Promise.resolve([]))
+}
