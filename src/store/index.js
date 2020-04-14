afa7f57b081c5005a7f0568f16cfb2f531814a1f
kKuna <faris@antcolony.io>
2020-04-14 13:04:00 +0200

    COMPLETED / Added redux-devtools-extension
    --------------------------------------------------------------------
     MAIN
     * Added redux-devtools-extension so we can track redux state changes in the react-native-debugger
    --------------------------------------------------------------------

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index'
 
-export default Store = createStore(rootReducer, applyMiddleware(thunk))
+export default Store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk),
+))
