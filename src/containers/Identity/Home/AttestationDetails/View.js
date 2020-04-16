bff9adcfdb0844f6c899650f43ed7b6c9dd5b6b2
Ant-Nazif <nazif.zuhric@antcolony.io>
2020-04-16 14:14:17 +0200

    CHECKPOINT/Fixes Attestion details

--- src/containers/Identity/Home/AttestationDetails/View.js
+++ src/containers/Identity/Home/AttestationDetails/View.js
@@ -1,39 +1,39 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Platform } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import { View, Text, Switch } from 'react-native';
 import styles from './styles';
-import { ScrollView } from 'react-native-gesture-handler';
 import QRCode from 'react-native-qrcode-svg';
-import { CheckBox } from 'react-native-elements'
 import Colors from '../../../../globals/colors'
+
+const awesomeLink = 'http://awesome.link.qr';
+
 const AttestationDetails = ({ navigation, actions }) => {
-    const [checked, setValue] = useState(false);
-    const setCheck=()=>{
-        if(checked)
-        setValue(false);
+    const [isEnabled, setIsEnabled] = useState(false);
+    const setEnable = () => {
+        if (isEnabled)
+            setIsEnabled(false);
         else
-        setValue(true);
+            setIsEnabled(true);
     }
     return (
         <View style={styles.root}>
-
-            <View style={{ alignItems: 'center', }}>
-                <View style={{backgroundColor:Colors.successButtonColor, padding:10, borderRadius:5}}>
-                <Text>COVID-19 Recovered</Text>
+            <View style={{ alignItems: 'center'}}>
+                <View style={{ backgroundColor: Colors.successButtonColor, paddingHorizontal: '14%', borderRadius: 5, paddingVertical: 16 }}>
+                    <Text style={{ color: 'white', fontSize: 15 }}>COVID-19 Recovered</Text>
                 </View>
-                <View style={{marginVertical:7}}>
-                <QRCode
-                    value="http://awesome.link.qr"
-                    logoSize={34}
-                />
+                <View style={{ marginVertical: '10%' }}>
+                    <QRCode
+                        value={awesomeLink}
+                        size={245}
+                    />
                 </View>
-                <CheckBox
-                    title='Pin attestation'
-                    checked={checked}
-                    onPress={setCheck}
-                    iconRight
-                    containerStyle={{backgroundColor:'white'}}
-                />
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Text style={{ paddingHorizontal: 10 }}>Pin attestation</Text>
+                    <Switch
+                        trackColor={{ false: "#767577", true: Colors.linkButtonColor }}
+                        onValueChange={setEnable}
+                        value={isEnabled}
+                    />
+                </View>
             </View>
 
         </View>
