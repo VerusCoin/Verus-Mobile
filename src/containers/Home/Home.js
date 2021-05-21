e79b2e84de0f92060e0277789d4f9cf5db1444dd
michaeltout <toutonghi.michael@gmail.com>
2021-05-21 22:32:10 +0200

    Fix display bug with fiat balance value

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -225,14 +225,14 @@ class Home extends Component {
               ? BigNumber(balances[key][wallet.id].total)
               : BigNumber("0")
           );
-
-          if (rates[key] && rates[key][displayCurrency]) {
-            const price = BigNumber(rates[key][displayCurrency])
-    
-            _totalFiatBalance = _totalFiatBalance.plus(coinBalances[coinObj.id].multipliedBy(price))
-          }
         }
       })
+
+      if (rates[key] && rates[key][displayCurrency]) {
+        const price = BigNumber(rates[key][displayCurrency])
+
+        _totalFiatBalance = _totalFiatBalance.plus(coinBalances[coinObj.id].multipliedBy(price))
+      }
     })
 
     return {
