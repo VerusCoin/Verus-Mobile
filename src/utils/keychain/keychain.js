7325c7c318ecffef3a88f332a0dcf13a8e323f62
michaeltout <toutonghi.michael@gmail.com>
2021-06-02 21:56:25 +0200

    Enable biometric auth on Android

--- src/utils/keychain/keychain.js
+++ src/utils/keychain/keychain.js
@@ -1,11 +1,15 @@
 import * as Keychain from 'react-native-keychain';
-import { INTERNAL_APP_ID } from '../../../env/index'
+import { INTERNAL_APP_ID, BIOMETRIC_SECURITY_THRESHOLD } from '../../../env/index'
 import { Platform } from 'react-native'
 
+const INCLUDE_SERVICE = Platform.OS === 'android'
 const SERVER_NAME = `${INTERNAL_APP_ID}_Session`
 
 export const getBiometricPassword = async (accountHash, title = "Authenticate to retreive password") => {
-  const credentials = await Keychain.getGenericPassword({
+  const credentials = await Keychain.getGenericPassword(INCLUDE_SERVICE ? {
+    service: 'com.verus.verusmobile',
+    authenticationPrompt: { title }
+  } : {
     authenticationPrompt: { title }
   });
 
@@ -14,7 +18,10 @@ export const getBiometricPassword = async (accountHash, title = "Authenticate to
 }
 
 export const getSessionPassword = async (title = "Authenticate Profile") => {
-  const credentials = await Keychain.getInternetCredentials(SERVER_NAME, {
+  const credentials = await Keychain.getInternetCredentials(SERVER_NAME, INCLUDE_SERVICE ? {
+    service: 'com.verus.verusmobile',
+    authenticationPrompt: { title }
+  } : {
     authenticationPrompt: { title }
   });
 
@@ -41,9 +48,20 @@ export const storeBiometricPassword = async (accountHash, password) => {
   let credentials = {}
 
   try {
-    const unparsedCredentials = await Keychain.getGenericPassword({
-      authenticationPrompt: { title: "Authenticate to store password in biometric keychain" }
-    })
+    const unparsedCredentials = await Keychain.getGenericPassword(
+      INCLUDE_SERVICE
+        ? {
+            service: "com.verus.verusmobile",
+            authenticationPrompt: {
+              title: "Authenticate to store password in biometric keychain",
+            },
+          }
+        : {
+            authenticationPrompt: {
+              title: "Authenticate to store password in biometric keychain",
+            },
+          }
+    );
 
     if (unparsedCredentials !== false) {
       credentials = JSON.parse(unparsedCredentials.password);
@@ -53,7 +71,11 @@ export const storeBiometricPassword = async (accountHash, password) => {
     throw e
   }
 
-  await Keychain.setGenericPassword(INTERNAL_APP_ID, JSON.stringify({ ...credentials, [accountHash]: password }), {
+  await Keychain.setGenericPassword(INTERNAL_APP_ID, JSON.stringify({ ...credentials, [accountHash]: password }), INCLUDE_SERVICE ? {
+    service: 'com.verus.verusmobile',
+    accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY,
+    accessible: Keychain.ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY
+  } : {
     accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY,
     accessible: Keychain.ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY
   })
@@ -63,7 +85,10 @@ export const removeBiometricPassword = async (accountHash) => {
   let credentials = {}
 
   try {
-    const unparsedCredentials = await Keychain.getGenericPassword({
+    const unparsedCredentials = await Keychain.getGenericPassword(INCLUDE_SERVICE ? {
+      service: 'com.verus.verusmobile',
+      authenticationPrompt: { title: "Authenticate to remove password in biometric keychain" }
+    } : {
       authenticationPrompt: { title: "Authenticate to remove password in biometric keychain" }
     })
 
@@ -74,7 +99,11 @@ export const removeBiometricPassword = async (accountHash) => {
 
   delete credentials[accountHash]
 
-  await Keychain.setGenericPassword(INTERNAL_APP_ID, JSON.stringify(credentials), {
+  await Keychain.setGenericPassword(INTERNAL_APP_ID, JSON.stringify(credentials), INCLUDE_SERVICE ? {
+    service: 'com.verus.verusmobile',
+    accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY,
+    accessible: Keychain.ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY
+  } : {
     accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY,
     accessible: Keychain.ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY
   })
@@ -118,11 +147,9 @@ export const getSupportedBiometryType = async () => {
 }
 
 export const passesSecurityThreshold = async () => {
-  if (Platform.OS == 'ios') return true
-  else return false
-  
-  //TODO: RE-Enable Biometry on Android when it can be forced to prompt every login
-  // const securityLevel = await Keychain.getSecurityLevel({ accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY })
+  const securityLevel = await Keychain.getSecurityLevel({
+    accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY,
+  });
 
-  // return securityLevel == Keychain.SECURITY_LEVEL[BIOMETRIC_SECURITY_THRESHOLD]
+  return securityLevel == Keychain.SECURITY_LEVEL[BIOMETRIC_SECURITY_THRESHOLD]
 }
