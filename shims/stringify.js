b592f5c8486ddf649d7a3436778517fbc09f29ac
michaeltout <toutonghi.michael@gmail.com>
2022-11-12 10:01:24 +0100

    Remove stringify shim

--- shims/stringify.js
+++ shims/stringify.js
@@ -19,31 +19,31 @@
 // and may prevent ETH transactions from displaying properly. If the cyclic structures 
 // bug returns, uncomment and fix the "replacer.call is not a function" bug on line 43. 
 
-function stringify(obj, replacer, spaces, cycleReplacer) {
-  return JSON.stringify(obj, serializer(replacer, cycleReplacer), spaces)
-}
+// function stringify(obj, replacer, spaces, cycleReplacer) {
+//   return JSON.stringify(obj, serializer(replacer, cycleReplacer), spaces)
+// }
 
-function serializer(replacer, cycleReplacer) {
-  var stack = [], keys = []
+// function serializer(replacer, cycleReplacer) {
+//   var stack = [], keys = []
 
-  if (cycleReplacer == null) cycleReplacer = function(key, value) {
-    if (stack[0] === value) return "[Circular ~]"
-    return "[Circular ~." + keys.slice(0, stack.indexOf(value)).join(".") + "]"
-  }
+//   if (cycleReplacer == null) cycleReplacer = function(key, value) {
+//     if (stack[0] === value) return "[Circular ~]"
+//     return "[Circular ~." + keys.slice(0, stack.indexOf(value)).join(".") + "]"
+//   }
 
-  return function(key, value) {
-    if (stack.length > 0) {
-      var thisPos = stack.indexOf(this)
-      ~thisPos ? stack.splice(thisPos + 1) : stack.push(this)
-      ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key)
-      if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value)
-    }
-    else stack.push(value)
+//   return function(key, value) {
+//     if (stack.length > 0) {
+//       var thisPos = stack.indexOf(this)
+//       ~thisPos ? stack.splice(thisPos + 1) : stack.push(this)
+//       ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key)
+//       if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value)
+//     }
+//     else stack.push(value)
 
-    return replacer == null ? value : replacer.call(this, key, value)
-  }
-}
+//     return replacer == null ? value : replacer.call(this, key, value)
+//   }
+// }
 
-JSON.origStringify = JSON.stringify;
+// JSON.origStringify = JSON.stringify;
 
-JSON.stringify = (obj, replacer, spaces, cycleReplacer) => JSON.origStringify(obj, serializer(replacer, cycleReplacer), spaces);
+// JSON.stringify = (obj, replacer, spaces, cycleReplacer) => JSON.origStringify(obj, serializer(replacer, cycleReplacer), spaces);
