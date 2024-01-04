4e1e18f6eddeb7c375b48c2a6a0940d5fab452ff
Chris <34682781+monkins1010@users.noreply.github.com>
2024-01-04 15:03:27 +0000

    Updates REV5 improve LinkID warnings

--- src/components/VerusIdObjectData.js
+++ src/components/VerusIdObjectData.js
@@ -1,101 +1,119 @@
 import React, { useEffect, useState } from 'react';
 import { Clipboard, FlatList, TouchableOpacity, Alert, View, Image } from 'react-native';
-import { Text, List, Divider, Button, Paragraph } from 'react-native-paper';
+import { Text, List, Divider, Paragraph } from 'react-native-paper';
 import Colors from '../globals/colors';
 import Styles from '../styles';
-import { VerusIdWallet, Revoke, Recover, Coins } from '../images/customIcons';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Revoke, Recover, Coins } from '../images/customIcons';
+import { openUrl } from "../utils/linking";
 import AnimatedSuccessCheckmark from "./AnimatedSuccessCheckmark";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const idWarningParams = [
-  {
-    icon: <Image source={Coins} style={{ aspectRatio: 1, height: 80 }} />,
-    title: "Spend and Sign",
-    text: ["Owned by you", "Funds can be spent by other addresses"]
-  },
-  {
-    icon: <Image source={Revoke} style={{ aspectRatio: 1.1, height: 80 }} />,
-    title: "Revoke",
-    text: ["Set to this ID", "Set to another ID"]
-  },
-  {
-    icon: <Image source={Recover} style={{ aspectRatio: 1, height: 80 }} />,
-    title: "Recover",
-    text: ["Set to this ID", "Set to another ID"]
-  },
-]
+const checkmark = (<AnimatedSuccessCheckmark style={{ width: 20, marginRight: 5, marginBottom: 1, alignSelf: 'center', }} />);
+
+const triangle = <MaterialCommunityIcons name={'information'} size={20} color={Colors.warningButtonColor} style={{ width: 20, marginRight: 9, alignSelf: 'center', }} />;
 
 export default function VerusIdObjectData(props) {
-  const { friendlyNames, verusId, StickyFooterComponent, flex } = props;
+  const { friendlyNames, verusId, StickyFooterComponent, flex, ownedByUser, ownedAddress } = props;
   const [listData, setListData] = useState([]);
-  const [idWarnings, setIdWarnings] = useState([true, false, true]);
-
   tryDisplayFriendlyName = iAddr => {
     return friendlyNames[iAddr] ? friendlyNames[iAddr] : iAddr;
   };
-
   copyDataToClipboard = (data, name) => {
     Clipboard.setString(data);
 
     Alert.alert(`${name} Copied`, `${data} copied to clipboard.`);
   };
 
-  useEffect(() => {
-    const tmpWarnings = [true, true, true];
-    if (verusId.identity.primaryaddresses.length > 1) {
-      tmpWarnings[0] = false;
+  const getWarningData = () => {
+    const tmpwarningData = [{
+      key: 'Spend and Sign',
+      data: 'Owned by you',
+      onPress: () => openUrl('https://docs.verus.io/verusid/#multisig'),
+      warning: false,
+      condition: 'warning',
+      icon: (<Image source={Coins} style={{ aspectRatio: 1, height: 50, width: 50, alignSelf: 'center' }} />),
+      status: checkmark
+    }, {
+      key: 'Revoke',
+      data: 'Set to this ID',
+      onPress: () => openUrl('https://docs.verus.io/verusid/#revoke-recover'),
+      warning: false,
+      condition: 'warning',
+      icon: <Image source={Revoke} style={{ aspectRatio: 1.1, height: 50, width: 50, alignSelf: 'center' }} />,
+      status: checkmark
+    }, {
+      key: 'Recover',
+      data: 'Set to this ID',
+      onPress: () => openUrl('https://docs.verus.io/verusid/#revoke-recover'),
+      warning: false,
+      condition: 'warning',
+      icon: <Image source={Recover} style={{ aspectRatio: 1, height: 50, width: 45, alignSelf: 'center' }} />,
+      status: checkmark
+    }];
+
+    if (verusId.identity.primaryaddresses.length > 1 || !verusId.identity.primaryaddresses.includes(ownedAddress)) {
+      tmpwarningData[0].warning = true;
+      tmpwarningData[0].data = 'Funds can be spent by other addresses';
+      tmpwarningData[0].status = triangle;
     }
 
-    if(verusId.identity.revocationauthority === verusId.identity.identityaddress) {
-      tmpWarnings[1] = false;
+    if (verusId.identity.revocationauthority === verusId.identity.identityaddress) {
+      tmpwarningData[1].warning = true;
+      tmpwarningData[1].data = 'Set to another ID';
+      tmpwarningData[1].status = triangle;
     }
 
-    if(verusId.identity.recoveryauthority === verusId.identity.identityaddress) {
-      tmpWarnings[2] = false;
+    if (verusId.identity.recoveryauthority === verusId.identity.identityaddress) {
+      tmpwarningData[2].warning = true;
+      tmpwarningData[2].data = 'Set to another ID';
+      tmpwarningData[2].status = triangle;
     }
-    setIdWarnings(tmpWarnings);
-  }, []);
+    return tmpwarningData;
+  };
 
   useEffect(() => {
     if (friendlyNames != null && verusId != null) {
-      let data = [
-        {
-          key: 'Name',
-          data: verusId.identity.name,
-          onPress: () => copyDataToClipboard(verusId.identity.name, 'Name'),
-        },
-        {
-          key: 'i-Address',
-          data: verusId.identity.identityaddress,
-          onPress: () =>
-            copyDataToClipboard(verusId.identity.identityaddress, 'Address'),
-        },
-        {
-          key: 'Status',
-          data: verusId.status,
-          capitalized: true,
-        },
-        {
-          key: 'Revocation Authority',
-          data: tryDisplayFriendlyName(verusId.identity.revocationauthority),
-          onPress: () =>
-            copyDataToClipboard(
-              verusId.identity.revocationauthority,
-              'Revocation',
-            ),
-        },
-        {
-          key: 'Recovery Authority',
-          data: tryDisplayFriendlyName(verusId.identity.recoveryauthority),
-          onPress: () =>
-            copyDataToClipboard(verusId.identity.recoveryauthority, 'Recovery'),
-        },
-        {
-          key: 'System',
-          data: tryDisplayFriendlyName(verusId.identity.systemid).replace(/@/g, ''),
-          onPress: () =>
-            copyDataToClipboard(verusId.identity.systemid, 'System ID'),
-        },
+
+      let warningData = ownedByUser ? getWarningData() : [];
+
+      let data = [...warningData,
+      {
+        key: 'Name',
+        data: verusId.identity.name,
+        onPress: () => copyDataToClipboard(verusId.identity.name, 'Name'),
+      },
+      {
+        key: 'i-Address',
+        data: verusId.identity.identityaddress,
+        onPress: () =>
+          copyDataToClipboard(verusId.identity.identityaddress, 'Address'),
+      },
+      {
+        key: 'Status',
+        data: verusId.status,
+        capitalized: true,
+      },
+      {
+        key: 'Revocation Authority',
+        data: tryDisplayFriendlyName(verusId.identity.revocationauthority),
+        onPress: () =>
+          copyDataToClipboard(
+            verusId.identity.revocationauthority,
+            'Revocation',
+          ),
+      },
+      {
+        key: 'Recovery Authority',
+        data: tryDisplayFriendlyName(verusId.identity.recoveryauthority),
+        onPress: () =>
+          copyDataToClipboard(verusId.identity.recoveryauthority, 'Recovery'),
+      },
+      {
+        key: 'System',
+        data: tryDisplayFriendlyName(verusId.identity.systemid).replace(/@/g, ''),
+        onPress: () =>
+          copyDataToClipboard(verusId.identity.systemid, 'System ID'),
+      },
       ];
 
       if (verusId.identity.privateaddress) {
@@ -132,40 +150,10 @@ export default function VerusIdObjectData(props) {
     Alert.alert(`${name} Copied`, `${data} copied to clipboard.`);
   };
 
-  const triangle = (
-    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-      <View style={{ width: 20, height: 20, marginRight: 5, marginTop: 5 }} />
-      <Icon name="exclamation-triangle" color="black" size={20} style={{ position: 'absolute' }} />
-      <Icon name="exclamation-triangle" color="yellow" size={18} style={{ position: 'absolute' }} />
-      <Icon name="exclamation" size={9} color="black" style={{ position: 'absolute' }} />
-    </View>
-  );
 
-  const checkmark = (<AnimatedSuccessCheckmark style={{ width: 20, marginRight: 5, marginTop: 1 }} />);
 
   return (
     <View style={flex ? { ...Styles.fullWidth, flex: 1 } : { ...Styles.fullWidth, height: "100%" }}>
-      <View
-        style={{
-          backgroundColor: 'white',
-          width: '100%',
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          paddingVertical: 20,
-        }}>
-        {idWarningParams.map((item, index) => {
-          return (
-            <View style={{ alignItems: 'center', width: "30%" }} key={index}>
-              {item.icon}
-              <Paragraph style={{ textAlign: 'center', textDecorationLine: 'underline', fontWeight: 'bold', marginTop: 10 }}>{item.title}</Paragraph>
-              <View style={{ flexDirection: 'row', marginTop: 10, alignItems: 'flex-start' }}>
-                  {idWarnings[index] ? checkmark : triangle}
-                <Paragraph style={{paddingHorizontal:5}}>{item.text[idWarnings[index] ? 0 : 1]}</Paragraph>
-              </View>
-            </View>
-          )
-        })}
-      </View>
       <FlatList
         style={flex ? { ...Styles.fullWidth, flex: 1 } : { ...Styles.fullWidth }}
         contentContainerStyle={{ paddingBottom: 152 }}
@@ -198,6 +186,40 @@ export default function VerusIdObjectData(props) {
                         </Text>
                       ) : null
                     }
+                  />
+                  <Divider />
+                </TouchableOpacity>
+              </React.Fragment>
+            );
+          } else if (item.condition == 'warning') {
+            return (
+              <React.Fragment>
+                <TouchableOpacity
+                  disabled={item.onPress == null}
+                  onPress={() => item.onPress()}>
+                  <List.Item
+                    title={item.data}
+                    description={item.key}
+                    titleNumberOfLines={item.numLines || 2}
+                    titleStyle={{ textAlign: 'left', color: item.warning ? 'red' : 'black', width: '90%' }}
+                    right={props =>
+                      <View style={{ width: '18%', flexDirection: 'row', alignContent: 'flex-end', justifyContent: 'flex-end', }}>
+                        {item.status}
+                        <Paragraph style={{
+                          fontSize: 12,
+                          textDecorationLine: 'underline',
+                          marginRight: 8,
+                          alignSelf: 'center',
+                          color: 'grey'
+                        }}>
+                          Learn more
+                        </Paragraph>
+                      </View>
+                    }
+                    left={() =>
+                      <View style={{ width: '15%', justifyContent: 'center', flexDirection: 'row' }}>
+                        {item.icon}
+                      </View>}
                   />
                   <Divider />
                 </TouchableOpacity>
