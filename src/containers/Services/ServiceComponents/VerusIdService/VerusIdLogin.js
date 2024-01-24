5c02f8c4323598e5889a09484838d08617b7a589
Chris <34682781+monkins1010@users.noreply.github.com>
2024-01-24 14:11:15 +0000

    Format VerusIdLogin.js

--- src/containers/Services/ServiceComponents/VerusIdService/VerusIdLogin.js
+++ src/containers/Services/ServiceComponents/VerusIdService/VerusIdLogin.js
@@ -1,19 +1,19 @@
 import { primitives } from 'verusid-ts-client'
-import { SET_DEEPLINK_DATA } from "../../../../utils/constants/storeType";
+import { SET_DEEPLINK_DATA } from "../../../../utils/constants/storeType"
 
 export const processVerusId = async (props, loginConsentRequest, fromService = null, fqn) => {
 
-    const req = new primitives.LoginConsentRequest();
-    req.fromBuffer(Buffer.from(loginConsentRequest, 'base64'));
+  const req = new primitives.LoginConsentRequest();
+  req.fromBuffer(Buffer.from(loginConsentRequest, 'base64'));
 
-    props.dispatch({
-      type: SET_DEEPLINK_DATA,
-      payload: {
-        id: primitives.LOGIN_CONSENT_REQUEST_VDXF_KEY.vdxfid,
-        data: req.toJson(),
-        fromService: fromService,
-        extraParams: {fqn}
-      },
-    });
-    props.navigation.navigate('DeepLink');
-  }
+  props.dispatch({
+    type: SET_DEEPLINK_DATA,
+    payload: {
+      id: primitives.LOGIN_CONSENT_REQUEST_VDXF_KEY.vdxfid,
+      data: req.toJson(),
+      fromService: fromService,
+      extraParams: { fqn }
+    },
+  });
+  props.navigation.navigate('DeepLink');
+}
