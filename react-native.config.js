7e8c3f22fab7818a9c62e88a737b5a83be4b0b4a
who-biz <37732338+who-biz@users.noreply.github.com>
2024-04-02 18:21:49 -0400

    remove android nullifier in react-native-config

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,7 @@ module.exports = {
   dependencies: {
     "react-native-verus-light-client": {
       platforms: {
-        android: null // disable Android platform, other platforms will still autolink if provided
+//        android: null // disable Android platform, other platforms will still autolink if provided
       }
     }
   },
