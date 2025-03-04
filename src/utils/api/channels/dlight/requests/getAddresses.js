1bb09abc6a6053a2a293687621efcc2f3f8c0a28
who-biz <37732338+who-biz@users.noreply.github.com>
2025-03-04 17:41:04 -0500

    Replace getAddresses request with Tools.deriveShieldedAddress for now
    - This fix is incomplete, and call to 'dispatch' is not handling response properly
    - Need to look into michael's prior work and emulate probably

--- src/utils/api/channels/dlight/requests/getAddresses.js
+++ src/utils/api/channels/dlight/requests/getAddresses.js
@@ -1,9 +1,12 @@
 import { makeDlightRequest } from '../callCreators'
 import { DLIGHT_PRIVATE_ADDRESSES } from '../../../../constants/dlightConstants'
+import { Tools } from 'react-native-verus'
 
 // Lists addresses associated with a light daemon wallet
-export const getAddresses = (coinId, accountHash, coinProto) => {
-  const res = makeDlightRequest(coinId, accountHash, coinProto, 0, DLIGHT_PRIVATE_ADDRESSES, [])
-  console.warn("getAddresses response: " + res)
+export const getAddresses = async (coinId, accountHash, coinProto, seed) => {
+  console.log("getAddresses: accountHash(" + accountHash + "), coinId(" + coinId + ")")
+  const res = await Tools.deriveShieldedAddress(seed, coinId)
+  //const res = makeDlightRequest(coinId, accountHash, coinProto, 0, DLIGHT_PRIVATE_ADDRESSES, [])
+  console.warn("getAddresses response: " + JSON.stringify(res))
   return res
 }
