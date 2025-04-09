545203b28a558483dbddd3ce5a93ebf4dfd1f084
who-biz <37732338+who-biz@users.noreply.github.com>
2025-04-09 17:30:14 -0500

    Use getLatestNetworkHeight for getBlockCount request

--- src/utils/api/channels/dlight/requests/getBlockCount.js
+++ src/utils/api/channels/dlight/requests/getBlockCount.js
@@ -1,7 +1,34 @@
 import { makeDlightRequest } from '../callCreators'
 import { DLIGHT_BLOCKCOUNT } from '../../../../constants/dlightConstants'
+import { getSynchronizerInstance } from 'react-native-verus'
+
 
 // Get the current chain blockheight
 export const getBlockCount = (coinId, accountHash, coinProto) => {
-  return makeDlightRequest(coinId, accountHash, coinProto, 0, DLIGHT_BLOCKCOUNT, [])
+  console.log(">>> getBlockCount called")
+  const sync = getSynchronizerInstance(accountHash, coinId);
+    let res, error = "";
+
+   return new Promise((resolve, reject) => {
+      synchronizer.getLatestNetworkHeight(accountHash)
+      .then(res => {
+        if (res.error != null) {
+          reject(
+            new ApiException(
+              res.error.message,
+              res.error.data,
+              coinId,
+              DLIGHT_PRIVATE,
+              res.error.code
+            )
+          );
+        } else {
+          //console.log("before jsonResponse")
+          //const jsonResponse = createJsonRpcResponse("1", res);
+          //console.log("getLatestNetworkHeight response: " res);
+          resolve(res);
+        }
+      })
+    })
+  //return makeDlightRequest(coinId, accountHash, coinProto, 0, DLIGHT_BLOCKCOUNT, [])
 }
