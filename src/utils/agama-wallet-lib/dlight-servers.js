a16330c0191a158ad74c6a47c84c2ac7753984ef
who-biz <37732338+who-biz@users.noreply.github.com>
2025-05-17 13:19:53 -0400

    Change dlight server to lightwalletd.whobiz.dev

--- src/utils/agama-wallet-lib/dlight-servers.js
+++ src/utils/agama-wallet-lib/dlight-servers.js
@@ -1,5 +1,5 @@
 let dlightServers = {
-  vrsc: ['lwdlegacy.blur.cash:443'],
+  vrsc: ['lightwalletd.whobiz.dev:443'],
   zec: ['lightwalletd.z.cash:9067'],
   zectest: ['lightwalletd.testnet.z.cash:9067']
 };
