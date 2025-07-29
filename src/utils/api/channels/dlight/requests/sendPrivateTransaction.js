dad172828682e849b5229b25d735c14f9c6b281d
who-biz <37732338+who-biz@users.noreply.github.com>
2025-07-29 13:13:46 -0400

    Get sendPrivateTransaction working with extsk

--- src/utils/api/channels/dlight/requests/sendPrivateTransaction.js
+++ src/utils/api/channels/dlight/requests/sendPrivateTransaction.js
@@ -1,12 +1,12 @@
-//import { DLIGHT_PRIVATE_SEND_PRIVATE_TX } from '../../../../constants/dlightConstants'
 import { preflightPrivateTransaction } from './preflightPrivateTransaction'
 import { coinsToSats } from '../../../../math'
 import BigNumber from 'bignumber.js'
 import { DLIGHT_PRIVATE } from '../../../../constants/intervalConstants'
-import { /*requestPrivKey,*/ requestSeeds } from '../../../../auth/authBox'
+import { requestSeeds } from '../../../../auth/authBox'
 import { createJsonRpcResponse } from './jsonResponse'
+import { isDlightSpendingKey } from '../../../../keys'
 
-import { getSynchronizerInstance, SpendInfo, SpendSuccess, SpendFailure } from 'react-native-verus'
+import { getSynchronizerInstance, SpendInfo, SpendSuccess, SpendFailure, Tools } from 'react-native-verus'
 
 // Sends a private transaction with given parameters
 export const sendPrivateTransaction = async (coinObj, activeUser, address, amount, params) => {
@@ -24,15 +24,17 @@ export const sendPrivateTransaction = async (coinObj, activeUser, address, amoun
       );
     }
 
-    //let spendingKey;
-    let mnemonicSeed;
+    let extsk = "";
+    let mnemonicSeed = "";
 
     try {
-      // TODO: we can use spendingKey here instead, but need to modify function,
-      // but need to modify function arguments for sendToAddress in RN module
-
-      //spendingKey = await requestPrivKey(coinObj.id, DLIGHT_PRIVATE)
-      mnemonicSeed = (await requestSeeds())[DLIGHT_PRIVATE];
+      let seed = (await requestSeeds())[DLIGHT_PRIVATE];
+      if (isDlightSpendingKey(seed)) {
+        extsk = await Tools.bech32Decode(seed);
+        //console.warn("in SendPrivateTransaction: extsk(" + extsk + ")");
+      } else {
+        mnemonicSeed = seed;
+      }
     } catch(e) {
       throw new Error(
         "Cannot spend transaction because user keys cannot be calculated."
@@ -48,6 +50,7 @@ export const sendPrivateTransaction = async (coinObj, activeUser, address, amoun
         zatoshi: coinsToSats(BigNumber(preflight.result.value)).toString(),
         toAddress: preflight.result.toAddress,
         memo: preflight.result.memo,
+        extsk: extsk,
         mnemonicSeed: mnemonicSeed
       }
       return new Promise((resolve, reject) => {
