5073767a5dfdaf9b1de657351b1644eea337dc62
michaeltout <toutonghi.michael@gmail.com>
2025-09-04 18:42:13 +0200

    Change react native config to prevent pod install error

--- react-native.config.js
+++ react-native.config.js
@@ -1,17 +1,10 @@
+// react-native.config.js
 module.exports = {
   dependencies: {
     "react-native-flipper": {
       platforms: {
-        ios: null // disable Android platform, other platforms will still autolink if provided
-      }
+        ios: null,
+      },
     },
-    "react-native-verus-light-client": {
-      platforms: {
-        android: null // disable Android platform, other platforms will still autolink if provided
-      }
-    }
   },
-  assets: [
-    "./assets/fonts/"
-  ],
-}
+};
