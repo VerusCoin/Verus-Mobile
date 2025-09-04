0fdb0dfffa7625fff274a4a1ceef8be37f06eb5b
michaeltout <toutonghi.michael@gmail.com>
2025-09-04 18:35:38 +0200

    Add changes to crypto as part of secure store changes

--- src/utils/crypto/crypto.js
+++ src/utils/crypto/crypto.js
@@ -9,21 +9,22 @@ const IV_LEN = 12;     // GCM standard
 const SALT_LEN = 16;
 const KEY_LEN = 32;    // 256-bit
 const TAG_LEN = 16;
-const PBKDF2_ITERS = 1000;
+const PBKDF2_ITERS = 100000;
+const PBKDF2_ITERS_MACHINE_GENERATED_KEY = 1;
 
 // Derive a key with PBKDF2-HMAC-SHA256
-function deriveKey(passphrase, salt) {
+function deriveKey(passphrase, salt, iters = PBKDF2_ITERS) {
   const key = pbkdf2(sha256, Buffer.from(passphrase, 'utf8'), salt, {
-    c: PBKDF2_ITERS,
+    c: iters,
     dkLen: KEY_LEN,
   });
 
   return Buffer.from(key);
 }
 
-export async function saltedEncrypt(passphrase, data) {
+export async function saltedEncrypt(passphrase, data, iters) {
   const salt = await randomBytes(SALT_LEN);
-  const key = deriveKey(passphrase, salt);
+  const key = deriveKey(passphrase, salt, iters);
   const iv = await randomBytes(IV_LEN);
 
   const cipher = crypto.createCipheriv(ALGO, key, iv);
@@ -35,14 +36,14 @@ export async function saltedEncrypt(passphrase, data) {
   return Buffer.concat([salt, iv, ct, tag]).toString('base64');
 }
 
-export function saltedDecrypt(passphrase, b64) {
+export function saltedDecrypt(passphrase, b64, iters) {
   const blob = Buffer.from(b64, 'base64');
   const salt = blob.subarray(0, SALT_LEN);
   const iv = blob.subarray(SALT_LEN, SALT_LEN + IV_LEN);
   const tag = blob.subarray(blob.length - TAG_LEN);
   const ct = blob.subarray(SALT_LEN + IV_LEN, blob.length - TAG_LEN);
 
-  const key = deriveKey(passphrase, salt);
+  const key = deriveKey(passphrase, salt, iters);
 
   const decipher = crypto.createDecipheriv(ALGO, key, iv);
   
@@ -50,4 +51,12 @@ export function saltedDecrypt(passphrase, b64) {
 
   const pt = Buffer.concat([decipher.update(ct), decipher.final()]);
   return pt.toString('utf8');
+}
+
+export async function saltedEncryptMGK(passphrase, data) {
+  return saltedEncrypt(passphrase, data, PBKDF2_ITERS_MACHINE_GENERATED_KEY);
+}
+
+export function saltedDecryptMGK(passphrase, b64) {
+  return saltedDecrypt(passphrase, b64, PBKDF2_ITERS_MACHINE_GENERATED_KEY)
 }
