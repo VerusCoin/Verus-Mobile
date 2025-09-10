3e138540e6c3756680bc5d97547dd3089e1c0b5a
who-biz <37732338+who-biz@users.noreply.github.com>
2025-09-10 15:02:35 -0700

    Fix error handling in getPrivateBalance

--- src/utils/api/channels/dlight/requests/getPrivateBalance.js
+++ src/utils/api/channels/dlight/requests/getPrivateBalance.js
@@ -7,33 +7,23 @@ import { satsToCoins} from '../../../../math'
 // Get the total private balance associated with a light wallet daemon
 export const getPrivateBalance = async (coinId, accountHash, coinProto) => {
   //console.log(">>> getPrivateBalance called")
-  const synchronizer = await getSynchronizerInstance(coinId, coinId);
+  const synchronizer = await getSynchronizerInstance(accountHash, coinId);
   return new Promise((resolve, reject) => {
     synchronizer.getPrivateBalance()
-      .then(res => {
-        if (res.error != null) {
-          reject(
-            new ApiException(
-              res.error.message,
-              res.error.data,
-              coinId,
-              DLIGHT_PRIVATE,
-              res.error.code
-            )
-          );
-        } else {
-          const transformedRes = {
-            ...res,
-            //TODO: simplify conditional below
-            ...(res && res.confirmed && res.total && res.pending && {
-              confirmed: satsToCoins(BigNumber(res.confirmed)),
-              total: satsToCoins(BigNumber(res.total)),
-              pending: satsToCoins(BigNumber(res.pending))
-            })
-          };
-          const result = createJsonRpcResponse(0, transformedRes, transformedRes.error)
-          resolve(result);
-        }
-      })
+    .then(res => {
+      const transformedRes = {
+        ...res,
+        //TODO: simplify conditional below
+        ...(res && res.confirmed && res.total && res.pending && {
+          confirmed: satsToCoins(BigNumber(res.confirmed)),
+          total: satsToCoins(BigNumber(res.total)),
+          pending: satsToCoins(BigNumber(res.pending))
+        })
+      };
+      const result = createJsonRpcResponse(0, transformedRes, transformedRes.error)
+      resolve(result);
+    }).catch(err => {
+      reject(error);
+    });
   })
 }
