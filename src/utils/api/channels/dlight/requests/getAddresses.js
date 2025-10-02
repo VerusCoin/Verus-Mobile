6a93111a8993d7dd196e1172e2689df08876f126
who-biz <37732338+who-biz@users.noreply.github.com>
2025-10-01 21:10:19 -0400

    Accomodate bech32decoding within (now) state-independent 'getAddresses'

--- src/utils/api/channels/dlight/requests/getAddresses.js
+++ src/utils/api/channels/dlight/requests/getAddresses.js
@@ -1,12 +1,13 @@
-import { getSynchronizerInstance, Tools } from 'react-native-verus'
+import { Tools } from 'react-native-verus'
 import { createJsonRpcResponse } from './jsonResponse'
-
+import { isDlightSpendingKey } from '../../../../keys'
 
 // Lists addresses associated with a light daemon wallet
 export const getAddresses = async (extsk, seed, coinId) => {
+  var spendingKeyHex = await Tools.bech32Decode(extsk);
   return new Promise((resolve, reject) => {
-    Tools.deriveShieldedAddress(extsk, seed, coinId)
-    .then(res => {   
+    Tools.deriveShieldedAddress(spendingKeyHex, seed, coinId)
+    .then(res => {
        const result = createJsonRpcResponse(0, res, null)
        resolve(result);
     })
