c080f510f88760a70ce4361476dc90f18b6b6d2f
who-biz <37732338+who-biz@users.noreply.github.com>
2025-10-02 23:17:15 -0400

    Fix error handling in getBlockCount

--- src/utils/api/channels/dlight/requests/getBlockCount.js
+++ src/utils/api/channels/dlight/requests/getBlockCount.js
@@ -3,27 +3,13 @@ import { getSynchronizerInstance } from 'react-native-verus'
 // Get the current chain blockheight
 export const getBlockCount = (coinId, accountHash, coinProto) => {
   const sync = getSynchronizerInstance(accountHash, coinId);
-    let res, error = "";
-
-   return new Promise((resolve, reject) => {
-      synchronizer.getLatestNetworkHeight(coinId)
+  return new Promise((resolve, reject) => {
+    synchronizer.getLatestNetworkHeight(coinId)
       .then(res => {
-        if (res.error != null) {
-          reject(
-            new ApiException(
-              res.error.message,
-              res.error.data,
-              coinId,
-              DLIGHT_PRIVATE,
-              res.error.code
-            )
-          );
-        } else {
-          //console.log("before jsonResponse")
-          //const jsonResponse = createJsonRpcResponse("1", res);
-          //console.log("getLatestNetworkHeight response: " res);
-          resolve(res);
-        }
+        resolve(res);
       })
-    })
+      .catch(err => {
+          reject(err);
+      });
+  })
 }
