7b63a3a5a484bcb48830357e6345e7022e6752d2
who-biz <37732338+who-biz@users.noreply.github.com>
2026-01-19 03:14:42 -0500

    Remove createJsonRpcResponse from getEncryptionAddress request entirely
    - Spoke with michael and we agree this does not seem necessary when we are outside of 'multi-coin-support' redux channels

--- src/utils/api/channels/dlight/requests/getEncryptionAddress.js
+++ src/utils/api/channels/dlight/requests/getEncryptionAddress.js
@@ -1,10 +1,9 @@
 import { ChannelKeysRequest, Tools } from 'react-native-verus';
-import { createJsonRpcResponse } from './jsonResponse'
 
 export const z_getencryptionaddress = async (seed, extsk, fromId, toId, hdIndex, encryptionIndex, returnSecret) => {
     const params: ChannelKeysRequest = {
         mnemonicSeed: seed,
-        extsk: extsk ? await Tools.bech32Decode(extsk) : extsk,
+        extsk: extsk ? await Tools.bech32Decode(extsk) : null,
         fromId,
         toId,
         hdIndex: hdIndex ?? -1,
@@ -14,7 +13,6 @@ export const z_getencryptionaddress = async (seed, extsk, fromId, toId, hdIndex,
     try {
         const res = await Tools.getVerusEncryptionAddress(params);
         return res;
-        //createJsonResponse(0, res, null); //TODO: verify we actually need this in JsonResponse format
    } catch (err) {
         throw err;
    }
