53c1c148cd445e7f22e965c97c6a6968dc6c1894
who-biz <37732338+who-biz@users.noreply.github.com>
2026-03-05 15:20:10 -0500

    Fix imports in encrypt.js

--- src/utils/api/channels/dlight/requests/encrypt.js
+++ src/utils/api/channels/dlight/requests/encrypt.js
@@ -1,10 +1,10 @@
-import { Tools } from 'react-native-zcash';
-import ApiException from '../../../../errors/apiError';
+import { Tools } from 'react-native-verus';
 import { DLIGHT_PRIVATE } from '../../../../constants/intervalConstants';
 
 
 export const encryptVerusMessage = async (address, message, returnSsk = false) => {
   try {
+      console.warn("Js encryptVerusMessage entered!")
       const payload = await Tools.encryptVerusMessage(address, message, returnSsk);
       return payload;
   } catch (err) {
