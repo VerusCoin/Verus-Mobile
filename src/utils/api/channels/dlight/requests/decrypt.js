1976f4419c65258d34e85bfbab6a6f4ba6b7d462
who-biz <37732338+who-biz@users.noreply.github.com>
2026-03-05 17:23:38 -0500

    Fix typo in named argument for decrypt

--- src/utils/api/channels/dlight/requests/decrypt.js
+++ src/utils/api/channels/dlight/requests/decrypt.js
@@ -2,7 +2,7 @@ import { Tools } from 'react-native-verus';
 import { DLIGHT_PRIVATE } from '../../../../constants/intervalConstants';
 
 
-export const decryptVerusMessage = async (fvkHex, epkHex, cipertextHex, sskHex) => {
+export const decryptVerusMessage = async (fvkHex, epkHex, ciphertextHex, sskHex) => {
     try {
         const res = await Tools.decryptVerusMessage(
             //TODO: I think we should handle default args elsewhere
