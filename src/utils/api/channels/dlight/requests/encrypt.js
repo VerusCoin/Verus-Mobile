a4a8f41157365eea3f825c05f40a8f41c1f38127
who-biz <37732338+who-biz@users.noreply.github.com>
2026-03-05 14:52:58 -0500

    Modify 'encrypt' request to properly use async

--- src/utils/api/channels/dlight/requests/encrypt.js
+++ src/utils/api/channels/dlight/requests/encrypt.js
@@ -3,18 +3,11 @@ import ApiException from '../../../../errors/apiError';
 import { DLIGHT_PRIVATE } from '../../../../constants/intervalConstants';
 
 
-export const encryptVerusMessage = async (alias, address, message, returnSsk = false) => {
+export const encryptVerusMessage = async (address, message, returnSsk = false) => {
   try {
-    const payload = await Tools.encryptVerusMessage(address, message, returnSsk);
-
-    return {
-      result: payload,
-      err: false
-    };
-  } catch (e) {
-    return {
-      err: true,
-      result: new ApiException(e.message, e.data, alias, DLIGHT_PRIVATE, e.code)
-    };
+      const payload = await Tools.encryptVerusMessage(address, message, returnSsk);
+      return payload;
+  } catch (err) {
+      throw err;
   }
 }
