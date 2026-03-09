bf93587af628fbd02b8ad17b336a9f7b5403b44b
who-biz <37732338+who-biz@users.noreply.github.com>
2026-03-08 17:38:56 -0700

    Update yarn.lock for latest react-native-verus

--- src/utils/api/channels/dlight/requests/encrypt.js
+++ src/utils/api/channels/dlight/requests/encrypt.js
@@ -4,7 +4,6 @@ import { DLIGHT_PRIVATE } from '../../../../constants/intervalConstants';
 
 export const encryptVerusMessage = async (address, message, returnSsk = false) => {
   try {
-      console.warn("Js encryptVerusMessage entered!")
       const payload = await Tools.encryptVerusMessage(address, message, returnSsk);
       return payload;
   } catch (err) {
